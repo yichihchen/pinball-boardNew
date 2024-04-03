bfbe2e473a548b7b092532eee02d3bc8f0891789
unknown <ingridiaim@gmail.com>
2024-04-03 14:20:22 +0800

    edis sound

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,8 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/pinball-boardnew/',
+  base: '/pinball-board/',
   plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
     moveOutputPlugin(),
   ],
