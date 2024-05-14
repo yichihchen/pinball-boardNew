3a9dc9b15f6cff7aac19eb7ce8cfff0c21ce6cc5
unknown <ingridiaim@gmail.com>
2024-05-14 20:32:26 +0800

    wasm

--- main.js
+++ main.js
@@ -2,4 +2,17 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './node_modules/nouislider/dist/nouislider.min.mjs';
+import './Decoder';
+import './Player';
+import './YUVCanvas';
+// import init from './avc.wasm?init';
 
+// init().then((instance) => {
+//   instance.exports.test()
+// })
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+export default defineConfig({
+  plugins: [wasm(), topLevelAwait()]
+});
