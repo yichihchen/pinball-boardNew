9ababcd8ed3f80a94ebbb278b95bfd7317d28ca6
unknown <ingridiaim@gmail.com>
2024-05-15 15:59:14 +0800

    Vite & js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 // import './node_modules/nouislider/dist/nouislider.min.mjs';
-// import './Decoder';
-// import './Player';
-// import './YUVCanvas';
+import './Decoder';
+import './Player';
+import './YUVCanvas';
